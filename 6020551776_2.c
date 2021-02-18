2116dec5e27512dc71641f55533e28b8c48a3b25
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-02-18 17:45:56 +0700

    2เต็ม

--- 6020551776_2.c
+++ 6020551776_2.c
@@ -1,25 +1,21 @@
+
 #include<stdio.h>
 int main()
 {
-	int n,i,j,max=0,sum;
-	scanf("%d" ,&n);
-	int a[n],b[n];
-	for(i=0;i<n;i++)
-	{
-		scanf("%d %d",&a[i],&b[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-		if(a[i]==a[i+1])
-		{
-			b[i]+=b[i+1];
-		}
-		if(b[i]>max)
-		{
-			max=b[i];
-			sum=i+1;
-		}
-	}
-	printf("%d %d",sum,max);
-	return 0;
+    int i,n,a,b,sum[48]={0},max=0,ans;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&a,&b);
+        sum[a-1]+=b;
+    }
+    for(i=0;i<48;i++)
+    {
+        if(max<sum[i])
+        {
+            max=sum[i];
+            ans=i;
+        }
+    }
+    printf("%d %d",ans+1,sum[ans]);
 }
