0adcbf885b5126fc8bad9d42a6a23e47f4d5a708
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-02-18 15:16:16 +0700

    1.2

--- 6020551776_1.c
+++ 6020551776_1.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int sum,i,j;
+	int sum,i,j,n,max=0,ans=0;
 	int a[5][4];
 	for(i=0;i<5;i++)
 	{
+		sum = 0;
 		for(j=0;j<4;j++)
 		{
 		    scanf("%d",&a[i][j]);
+		    sum+=n;
 		}
 	}
+	if(sum>max)
+	{
+		max=sum;
+		
+	}
+	
+	
 }
